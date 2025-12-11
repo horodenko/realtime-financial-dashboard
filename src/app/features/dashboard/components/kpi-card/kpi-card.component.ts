45ef10df9a36bc6461591c11d09a1edc9c21b302
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2025-12-10 22:58:19 -0300

    feat | KpiCardComponent - setting up component

--- src/app/features/dashboard/components/kpi-card/kpi-card.component.ts
+++ src/app/features/dashboard/components/kpi-card/kpi-card.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
-  selector: 'app-kpi-card.component',
-  imports: [],
+  selector: 'app-kpi-card',
+  imports: [DecimalPipe],
   templateUrl: './kpi-card.component.html',
   styleUrl: './kpi-card.component.scss',
 })
 export class KpiCardComponent {
-
+  public title = input<string>('');
+  public value = input<number>(0);
 }
