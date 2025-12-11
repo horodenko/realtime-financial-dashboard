4bb0114dc199cafb396e45f3980c491f38bd1d9c
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2025-12-10 22:58:03 -0300

    feat | DashboardComponent - setting up component

--- src/app/features/dashboard/pages/dashboard.component.ts
+++ src/app/features/dashboard/pages/dashboard.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FinanceService } from '../../../core/services/finance.service';
+import { KpiCardComponent } from '../components/kpi-card/kpi-card.component';
+import { RealtimeChartComponent } from '../components/realtime-chart/realtime-chart.component';
 
 @Component({
   selector: 'app-dashboard.component',
-  imports: [],
+  imports: [KpiCardComponent, RealtimeChartComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  private _finance = inject(FinanceService);
 
+  protected stats: any;
+
+  ngOnInit(): void {
+    this._finance.stats$.subscribe(stats => this.stats = stats);
+  }
 }
