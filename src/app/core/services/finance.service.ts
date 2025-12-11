8a66e7d53abc6f91f704bd00bddbafbb6d3aaeca
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2025-12-10 22:59:20 -0300

    fix | FinanceService - setting up main observable responsible for controlling dynamic data on dashboard

--- src/app/core/services/finance.service.ts
+++ src/app/core/services/finance.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FinanceService {
-  
+  stats$ = interval(2000).pipe(
+    map(() => ({
+      balance: 3500 + Math.random() * 300,
+      income: 1200 + Math.random() * 200,
+      expenses: 800 + Math.random() * 150
+    }))
+  );
 }
