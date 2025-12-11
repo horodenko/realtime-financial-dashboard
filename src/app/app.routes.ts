dec013b7aaa06fe52a7c185844d1bf9614d8b524
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2025-12-10 22:58:31 -0300

    Chore | setting up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/pages/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
