4ee95253941e5eec752d5db0a8b9ee7e8f9d9b43
Daniel Horodenko Magno da Silva <danhorodenko@Daniels-MacBook-Air.local>
2025-12-10 22:42:43 -0300

    feature | setting up project

---
+++ finance-dashboard/src/app/features/dashboard/components/realtime-chart/realtime-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-realtime-chart.component',
+  imports: [],
+  templateUrl: './realtime-chart.component.html',
+  styleUrl: './realtime-chart.component.scss',
+})
+export class RealtimeChartComponent {
+
+}
